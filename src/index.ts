7507cbbec3d83067a3479f0f873173c493d4a368
Nikita Sokolov <socnikvlad@gmail.com>
2024-04-09 16:50:09 +0300

    feat: setup typescript and tsx

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Write your code here.
+// In this template you can create more than one module.
+// Just create new files, import it and enjoy!
+// "@" path alias is supported
+
+console.log('Hello, World!')
